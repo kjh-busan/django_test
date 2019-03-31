c758500aff68e560d334a7c5e4ff3b16bd9d11e7
bit-ksw <bit7521@gmail.com>
2019-03-31 23:43:47 +0900

    게시글 상세화면 구현하기

--- third/urls.py
+++ third/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('list/', views.list, name="list"),
-    path('create/', views.create, name="restaurant-create")
+    path('create/', views.create, name="restaurant-create"),
+    path('update/', views.update, name="restaurant-update"),
+    path('detail/', views.detail, name="restaurant-detail"),
 ]
--- third/views.py
+++ third/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from third.models import Restaurant
 from django.core.paginator import Paginator
 from third.forms import RestaurantForm
 from django.http import HttpResponseRedirect
+
 
 
 # Create your views here.
@@ -27,3 +28,25 @@ def create(request):
         return HttpResponseRedirect('/third/list/')
     form = RestaurantForm()
     return render(request, 'third/create.html', {'form': form})
+
+
+def update(request):
+    if request.method == 'POST' and 'id' in request.POST:
+        #item = Restaurant.objects.get(pk=request.POST.get('id'))
+        item = get_object_or_404(Restaurant, pk=request.POST.get('id'))
+        form = RestaurantForm(request.POST, instance=item)
+        if form.is_valid():
+            item = form.save()
+    elif request.method == 'GET':
+        #item = Restaurant.objects.get(pk=request.GET.get('id'))
+        item = get_object_or_404(Restaurant, pk=request.GET.get('id'))
+        form = RestaurantForm(instance=item)
+        return render(request, 'third/update.html', {'form': form})
+    return HttpResponseRedirect('/third/list/')
+
+
+def detail(request):
+    if 'id' in request.GET:
+        item = get_object_or_404(Restaurant, pk=request.GET.get('id'))
+        return render(request, 'third/detail.html', {'item': item})
+    return HttpResponseRedirect('third/list/')
