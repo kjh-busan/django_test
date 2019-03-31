03fd59272b93bf77ad5b987c4367ee2b8aa82520
bit-ksw <bit7521@gmail.com>
2019-03-31 16:09:40 +0900

    페이지구현하기

--- third/views.py
+++ third/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from third.models import Restaurant
+from django.core.paginator import Paginator
+
 
 # Create your views here.
 def list(request):
+    restaurants = Restaurant.objects.all()
+    paginator = Paginator(restaurants, 5)
+
+    page = request.GET.get('page')
+    items = paginator.get_page(page)
+
     context = {
-        'restaurants': Restaurant.objects.all()
+        'restaurants': items
     }
+
     return render(request, 'third/list.html', context)
